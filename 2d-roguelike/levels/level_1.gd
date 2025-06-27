c9329e67f4649ce2661aa642d4f3cb33f6ebe9eb
Ninjobi <theimaninja@gmail.com>
2025-06-27 17:55:56 -0400

    Made a timeout to stop the game from ending right away after the last enemy is killed

--- 2d-roguelike/levels/level_1.gd
+++ 2d-roguelike/levels/level_1.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 @onready var game_over_modal = $GameOver
+var time_limit = 5.0  # seconds
+var elapsed = 0.0
+var done = false
 
 func _ready() -> void:
 	$AudioPlayer.play()
 
 func _on_monster_spawner_all_monsters_killed() -> void:
+	await get_tree().create_timer(2.0).timeout
 	Game.level_complete()
 
 func _on_player_player_died() -> void:
