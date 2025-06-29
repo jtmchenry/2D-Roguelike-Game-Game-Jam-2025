752b03c289212ad4ed2c404318cb65777048ca02
Ninjobi <theimaninja@gmail.com>
2025-06-29 19:09:25 -0400

    Bossing

---
+++ 2d-roguelike/ui/hud/boss_health_bar.gd
@@ -0,0 +1,24 @@
+extends ProgressBar
+class_name BossHealthBar
+
+var max_health: int = 0
+var current_health: int = 0
+@onready var health_src: Health = $SlimeKingHealth
+@onready var label: Label = $Label
+
+func _ready() -> void:
+	current_health = health_src.health
+	max_health = health_src.max_health
+	health_src.connect("health_changed", _handle_health_change)
+
+func _process(delta) -> void:
+	label.text = "%s / %s" % [current_health, max_health]
+	if max_health == 0:
+		queue_free()
+		return
+	value = current_health * 100 / max_health
+	return 
+	
+func _handle_health_change(current: int, max: int):
+	current_health = current
+	max_health = max
