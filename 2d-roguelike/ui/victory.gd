6689bff2ebd73e5671c2101d22023d04a922b884
Ninjobi <theimaninja@gmail.com>
2025-06-30 00:15:40 -0400

    Last Minute Changes

--- 2d-roguelike/ui/victory.gd
+++ 2d-roguelike/ui/victory.gd
@@ -7,3 +7,12 @@ func _on_play_again_pressed() -> void:
 func reset():
 	Game.current_level = 1
 	Player1.reset_player_stats()
+
+
+func _on_title_screen_pressed() -> void:
+	reset()
+	get_tree().change_scene_to_file("res://ui/main_menu.tscn")
+
+func _on_quit_pressed() -> void:
+	reset()
+	get_tree().quit()
